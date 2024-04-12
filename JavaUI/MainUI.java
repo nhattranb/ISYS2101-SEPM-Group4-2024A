110b7faaace4e14b4c164cdf7784cc2bfa477f57
nhattranb <s3926629@rmit.edu.vn>
2024-04-12 09:08:55 +0700

    added the Java-based UI version

---
+++ JavaUI/MainUI.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MainUI {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the HealthyPal application");
+        System.out.println("This is the menu for user" + User.getName());
+        System.out.println("1. View Profile");
+    }
+}
